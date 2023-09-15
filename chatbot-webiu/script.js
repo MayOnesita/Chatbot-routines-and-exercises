eca1c359115a9de92f613da07ba1ab27238a5507
MayOnesita <may.oropeza01@gmail.com>
2023-09-15 15:06:53 -0400

    16 - Fixed details

--- chatbot-webiu/script.js
+++ chatbot-webiu/script.js
@@ -7,7 +7,7 @@ let userMessage;
 const createChatLi = (message, className) => {
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", className);
-    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">fitness_center</span><p></p>`;
     chatLi.innerHTML = chatContent;
     chatLi.querySelector("p").textContent = message;
     return chatLi;
